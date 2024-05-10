1ff1dbc3b65d0b616dc93657d921a6b6409c987b
liukangjie <wodetiana_lkj@163.com>
2024-05-10 22:10:46 +0800

    update mod

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/antchfx/xpath v1.3.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
@@ -32,5 +33,5 @@ require (
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
-	google.golang.org/protobuf v1.34.0 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
 )
--- utils/file.go
+++ utils/file.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func WriteToFile(fileName string, content string, dir ...string) {
@@ -19,6 +20,31 @@ func WriteToFile(fileName string, content string, dir ...string) {
 	}
 	defer file.Close()
 	_, err = file.WriteString(content)
+	if err != nil {
+		fmt.Println(err)
+
+	}
+}
+
+func WriteToFileBytes(fileName string, content []byte, dir ...string) {
+	var path string
+	if len(dir) > 0 {
+		dir = append(dir, fileName)
+		path = Concat(dir...)
+	} else {
+		path = fileName
+	}
+
+	// 创建目录（如果需要）
+	dirPath := filepath.Dir(path)
+	_ = os.MkdirAll(dirPath, 0755)
+
+	var file, err = os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	_, err = file.Write(content)
 	if err != nil {
 		fmt.Println(err)
 
